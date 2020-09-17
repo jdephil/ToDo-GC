496e77180f65d363217a14b5ec3dacf8593feb05
jdephil <jennifer.dephillips@gmail.com>
2020-09-17 18:47:56 -0400

    task and date done, svgs in progress

--- main.js
+++ main.js
@@ -20,13 +20,31 @@ document.addEventListener('DOMContentLoaded', function() {
     e.preventDefault();
     let nameValue = taskName.value
     let dateValue = taskDate.value
-    console.log(dateValue)
     let taskLi = document.createElement("li")
+    let checkSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9E9E9E" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z"/>
+                        <circle cx="12" cy="12" r="9" />
+                        <path d="M9 12l2 2l4 -4" />
+                      </svg>`
+
+    let xSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F44336" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z"/>
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>`
+
     nameNode = document.createTextNode(nameValue)
     dateNode = document.createTextNode(dateValue)
-    console.log(nameNode, dateNode)
+    checkDiv = document.createElement('div')
+    
+    // checkBtn.classList.add('checkBtn');
+    checkDiv.innerHtml = checkSvg
+    
+    console.log(checkDiv)
+    
     taskLi.appendChild(nameNode)
     taskLi.appendChild(dateNode)
+    taskLi.appendChild(checkDiv)
     todoUl.appendChild(taskLi)
   }
 
