3eb79ac3c651aecd8fee8c9a1931ca7dfdd72ad3
jdephil <jennifer.dephillips@gmail.com>
2020-09-17 18:23:04 -0400

    show form done, adding task in progress

--- main.js
+++ main.js
@@ -6,5 +6,29 @@ document.addEventListener('DOMContentLoaded', function() {
   let taskDate = document.getElementById('taskDate')
   let todoUl = document.getElementById("todoList")
 
+  /*---FUNCTIONS---*/
 
+  let showForm = () => {    
+    if (formDiv.style.display === "initial" || "") {
+      formDiv.style.display = "none"
+    } else {formDiv.style.display = "initial"}
+ }
+
+  addBtn.addEventListener('click', showForm)
+
+  let addTask = (e) => {
+    e.preventDefault();
+    let nameValue = taskName.value
+    let dateValue = taskDate.value
+    console.log(dateValue)
+    let taskLi = document.createElement("li")
+    nameNode = document.createTextNode(nameValue)
+    dateNode = document.createTextNode(dateValue)
+    console.log(nameNode, dateNode)
+    taskLi.appendChild(nameNode)
+    taskLi.appendChild(dateNode)
+    todoUl.appendChild(taskLi)
+  }
+
+  document.getElementById("submitBtn").addEventListener("click", addTask)
 })
