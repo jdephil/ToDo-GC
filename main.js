f826d2a4c27bd35d4d8d2e6e7e326a1a4a2e3fbd
jdephil <jennifer.dephillips@gmail.com>
2020-09-17 17:53:12 -0400

    started js

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  /*---DOM refs---*/
+  let addBtn = document.getElementById('addBtn')
+  let formDiv = document.getElementById('formDiv')
+  let taskName = document.getElementById('taskName')
+  let taskDate = document.getElementById('taskDate')
+  let todoUl = document.getElementById("todoList")
+
+
+})
