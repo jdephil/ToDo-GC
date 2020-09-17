d2f620ef3603478769a4f18b7b7b9dd2c8db7656
jdephil <jennifer.dephillips@gmail.com>
2020-09-17 19:24:59 -0400

    add task done

--- main.js
+++ main.js
@@ -21,34 +21,20 @@ document.addEventListener('DOMContentLoaded', function() {
     let nameValue = taskName.value
     let dateValue = taskDate.value
     let taskLi = document.createElement("li")
-    let checkSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9E9E9E" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z"/>
-                        <circle cx="12" cy="12" r="9" />
-                        <path d="M9 12l2 2l4 -4" />
-                      </svg>`
-
-    let xSvg = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#F44336" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z"/>
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>`
 
     nameNode = document.createTextNode(nameValue)
     dateNode = document.createTextNode(dateValue)
-    checkDiv = document.createElementNS(`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-check" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#9E9E9E" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    <path stroke="none" d="M0 0h24v24H0z"/>
-    <circle cx="12" cy="12" r="9" />
-    <path d="M9 12l2 2l4 -4" />
-  </svg>`, "svg")
+    checkEl = document.createElement('i')
+    deleteEl = document.createElement('i')
     
-     taskLi.classList.add('card', "liItems");
-    // checkDiv.innerHtml = checkSvg
+    taskLi.classList.add('card', "liItems");
+    checkEl.classList.add('far', 'fa-check-circle')
+    deleteEl.classList.add('fa', 'fa-times')
     
-    console.log(checkDiv)
-    
+    taskLi.appendChild(checkEl)
     taskLi.appendChild(nameNode)
     taskLi.appendChild(dateNode)
-    taskLi.appendChild(checkDiv)
+    taskLi.appendChild(deleteEl)
     todoUl.appendChild(taskLi)
   }
 
