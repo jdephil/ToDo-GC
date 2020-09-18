52f229ca07de22c990b032bf2a2fe161a44fa2f8
jdephil <jennifer.dephillips@gmail.com>
2020-09-17 20:09:12 -0400

    working on completed task, before dinner save

--- main.js
+++ main.js
@@ -24,19 +24,60 @@ document.addEventListener('DOMContentLoaded', function() {
 
     nameNode = document.createTextNode(nameValue)
     dateNode = document.createTextNode(dateValue)
+    checkDiv = document.createElement('div')
     checkEl = document.createElement('i')
     deleteEl = document.createElement('i')
     
     taskLi.classList.add('card', "liItems");
     checkEl.classList.add('far', 'fa-check-circle')
     deleteEl.classList.add('fa', 'fa-times')
+    checkDiv.classList.add('checkDiv')
+
+    checkDiv.addEventListener('click', completeTask)
     
-    taskLi.appendChild(checkEl)
+    checkDiv.appendChild(checkEl)
+    taskLi.appendChild(checkDiv)
     taskLi.appendChild(nameNode)
     taskLi.appendChild(dateNode)
     taskLi.appendChild(deleteEl)
     todoUl.appendChild(taskLi)
+
+    
   }
 
   document.getElementById("submitBtn").addEventListener("click", addTask)
+
+  let checkCircle = document.getElementsByClassName('fa-check-circle')
+
+
+  let completeTask = () => {
+    console.log(checkCircle)
+    if (checkDiv.children.style.color === "rgb(158, 158, 158)" || "") {
+      checkDiv.children.style.color = "green"
+    } else {checkDiv.children.style.color = "rgb(158, 158, 158)"}
+  }
+  
+  // checkCircle.addEventListener('click', completeTask)
+
+  // checkCircleDiv = document.querySelectorAll('.checkDiv')
+  
+  // document.addEventListener('click', function(e) {
+  //   if(/checkDiv/.test(e.target.className)) {
+  //     console.log('clicked')
+  //   }
+  // }, false);  
+
+  // .forEach(item => {
+  //   item.addEventListener('click', event => {
+  //     console.log('clicked')
+  //     if (checkCircle.style.color === "rgb(158, 158, 158)" || "") {
+  //       checkCircle.style.color = "green"
+  //     } else {checkCircle.style.color = "rgb(158, 158, 158)"}
+  //   })
+  // })
+
+
+
+
+
 })
